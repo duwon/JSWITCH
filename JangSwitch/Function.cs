18a6a71c1bb1f31ad3333290b89a098aa193c276
duwon <duwon@duwon.net>
2020-03-18 01:36:25 +0900

    기본 프로젝트 생성

---
+++ JangSwitch/Function.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JangSwitch
+{
+    public partial class Function : Form
+    {
+        public Function(string[] args)
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+
+        }
+
+            bool button_state = true;
+        private void Button1_Click(object sender, MouseEventArgs e)
+        {
+
+            if (button_state == true)
+            {
+                button1.Image = Properties.Resources.btn_start;
+                button_state = false;
+            }
+            else
+            {
+                button1.Image = Properties.Resources.btn_stop;
+                button_state = true;
+            }
+        }
+    }
+}
